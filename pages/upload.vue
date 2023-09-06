cc6512e7691355613b3d47ae137d4dc36a17db2c
isfahan <isfahan.khan@bjss.com>
2023-09-06 13:32:25 +0100

    upload: add base64 function

--- pages/upload.vue
+++ pages/upload.vue
@@ -1,11 +1,49 @@
 <template>
     <div class="file-upload">
       <div class="file-upload__area">
-        <input type="file" name="" id="" />
+        <input type="file" name="" id="" @change="encodeImageToBase64" />
+        <textarea v-if="base64Image" :value="base64Image" rows="10" cols="30"></textarea>
       </div>
     </div>
   </template>
-   
+
+<script>
+export default {
+  data() {
+    return {
+      base64Image: null
+    };
+  },
+  methods: {
+    encodeImageToBase64(event) {
+      if (event.target.files && event.target.files[0]) {
+        let file = event.target.files[0];
+        let reader = new FileReader();
+
+        // reader.onload = (e) => {
+        //   console.log(e.target.result);
+        // };
+
+        reader.onload = function(e) {
+      // e.target.result contains the binary string
+      let binaryString = e.target.result;
+      let base64String = btoa(binaryString);
+      //console.log(base64String);
+
+      if (e.target.result === atob(base64String)) {
+          console.log("yo")
+        }
+    };
+
+
+
+        reader.readAsDataURL(file);
+      }
+    }
+  }       
+}; 
+</script>
+
   <style scoped>
   .file-upload {
     height: 100vh;
