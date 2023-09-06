a002e01b34e3e4e2a194194dbd1c0dedab543510
Sandy Lamond <sandy.lamond@bjss.com>
2023-09-06 15:00:11 +0100

    add pics to the gallery

--- pages/view.vue
+++ pages/view.vue
@@ -1,3 +1,11 @@
 <template>
-  <p>View some photos</p>
+<div v-for="image in images">
+  <img :src="image" alt="dolphin" style="width:300px;">
+</div>
+
 </template>
+
+<script setup>
+const images = ["dolphin.jpg", "lion.webp", "Monkey.webp", "elephant.jpg"]
+
+</script>
