46bd2a7e4efe1feb54969a54d3c80e0801934e0c
charlotte paul <charlotte.paul@bjss.com>
2023-09-06 15:34:44 +0100

    added about the challange

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About The Challange</h1>
+    <p>Developing a cloud-based website to display images is a dynamic and collaborative endeavor, 
+      and it brings with it a unique set of challenges. Ollie, David, Charlotte, Kate, Matt, Jaimie, and Kalista, 
+      the creators of this ambitious project, have encountered numerous obstacles along the way.</p>
+      <p>
+
+      Ollie, David, Charlotte, Kate, Matt, Jaimie, and Kalista 
+      navigated these challenges with determination and ingenuity,
+      ultimately creating a cloud-based image display website that not only met these challenges
+       but also provided users with a seamless and secure image viewing experience. 
+       Their collaboration and innovative problem-solving skills have paved the way for an impressive digital platform.
+      </p>
   </div>
 </template>
+
+
